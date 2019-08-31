4a89e23fe4c2d08562a582b6825388bd50f04e74
Charles Patterson Cook <cook.char.p@gmail.com>
2019-08-31 00:37:36 -0400

    Added topple! function

--- ccSandpiles.jl
+++ ccSandpiles.jl
@@ -1,16 +1,6 @@
 module ccSandpiles
 	import Base.show
-	export Sandpile
-
-	function topple!(p::Sandpile)
-		local sites = Array{Array{Integer, 1}}(undef, 0)
-		local i, j
-		for i = 1:size(p.pile)[1]
-			for j = 1:size(p.pile)[2]
-				p.pile[i,j] > 3 ? push!(sites, [i,j]) : continue
-			end
-		end
-	end
+	export Sandpile, topple!
 
 	mutable struct Sandpile
 		# Multidim. Array for holding pile data
@@ -37,5 +27,27 @@ module ccSandpiles
 				j == size(p.pile)[2] ? print('\n') : print(' ')
 			end
 		end
+	end
+
+	function topple!(p::Sandpile)
+		local sites = Array{Array{Integer, 1}}(undef, 0)
+		local i, j
+		for i = 1:size(p.pile)[1]
+			for j = 1:size(p.pile)[2]
+				p.pile[i,j] > 3 ? push!(sites, [i,j]) : continue
+			end
+		end
+		if length(sites) == 0 return end
+		local x, y
+		for i = 1:length(sites)
+			y = sites[i][1]
+			x = sites[i][2]
+			p.pile[y, x] -= 4
+			if y - 1 > 0 p.pile[y - 1, x] += 1 end
+			if y < size(p.pile)[1] p.pile[y + 1, x] += 1 end
+			if x - 1 > 0 p.pile[y, x - 1] += 1 end
+			if x < size(p.pile)[2] p.pile[y, x + 1] += 1 end
+		end
+		println(p)
 	end
 end
