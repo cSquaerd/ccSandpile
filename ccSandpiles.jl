45e7c94cf8baa391376d3c8880a635830e7f13d7
Charles Patterson Cook <cook.char.p@gmail.com>
2019-08-31 01:34:30 -0400

    Added deposit!,maximum, and fullyTopple! functions

--- ccSandpiles.jl
+++ ccSandpiles.jl
@@ -1,35 +1,39 @@
 module ccSandpiles
-	import Base.show
-	export Sandpile, topple!
+	import Base.show, Base.maximum
+	export Sandpile, topple!, deposit!, fullyTopple!
 
 	mutable struct Sandpile
 		# Multidim. Array for holding pile data
-		pile::Array{UInt8, 2}
+		pile::Array{UInt16, 2}
 		# Explicit constructor
-		function Sandpile(p::Array{UInt8, 2})
+		function Sandpile(p::Array{Int, 2})
 			local i, j
+			local q = similar(p, UInt16)
 			for i = 1:size(p)[1]
 				for j = 1:size(p)[2]
-					p[i, j] > 3 ? throw(ArgumentError("Initial piles must be fully toppled!\n")) : continue
+					p[i, j] > 3 ? throw(ArgumentError("Initial piles must be fully toppled!\n")) : q[i, j] = p[i, j]
 				end
 			end
-			new(p)
+			new(q)
 		end
 		# Implicit constructor
-		Sandpile(w::Integer, h::Integer, empty::Bool=true) = empty ? new(zeros(UInt8, UInt(w), UInt(h))) : new(Array{UInt8}(undef, UInt(w), UInt(h)))
+		Sandpile(w::Integer, h::Integer, empty::Bool=true) = empty ? new(zeros(UInt16, UInt(w), UInt(h))) : new(Array{UInt16}(undef, UInt(w), UInt(h)))
 	end
 
 	function Base.show(io::IO, p::Sandpile)
 		local i, j
+		local saturated = false
+		local colors = [:cyan, :blue, :magenta, :red]
 		for i = 1:size(p.pile)[1]
 			for j = 1:size(p.pile)[2]
-				printstyled(io, p.pile[i,j], color = :blue)
+				saturated = p.pile[i,j] > 3
+				printstyled(io, p.pile[i,j], bold = saturated, color = !saturated ? colors[p.pile[i,j] + 1] : :light_black)
 				j == size(p.pile)[2] ? print('\n') : print(' ')
 			end
 		end
 	end
 
-	function topple!(p::Sandpile)
+	function topple!(p::Sandpile, printout::Bool = false)
 		local sites = Array{Array{Integer, 1}}(undef, 0)
 		local i, j
 		for i = 1:size(p.pile)[1]
@@ -48,6 +52,12 @@ module ccSandpiles
 			if x - 1 > 0 p.pile[y, x - 1] += 1 end
 			if x < size(p.pile)[2] p.pile[y, x + 1] += 1 end
 		end
-		println(p)
+		if printout println(p) end
 	end
+
+	deposit!(p::Sandpile, amount::Integer, y::Integer, x::Integer) = p.pile[y, x] += amount
+
+	maximum(p::Sandpile) = maximum(p.pile)
+
+	fullyTopple!(p::Sandpile, printout::Bool = false) = while maximum(p) > 3 topple!(p, printout) end
 end
