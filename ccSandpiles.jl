b4497bd6977757efd913228bdd9ec51613ccb589
Charles Patterson Cook <cook.char.p@gmail.com>
2019-08-31 20:05:30 -0400

    Added imaging functions and switched to unsigned 32 bit ints for piles (up from unsigned 16 bit ints)

--- ccSandpiles.jl
+++ ccSandpiles.jl
@@ -1,15 +1,15 @@
 module ccSandpiles
-	import Base.show, Base.maximum, Base.size, Base.+, Base.*
-	export Sandpile, topple!, deposit!, fullyTopple!, isStable
+	import Base.show, Base.maximum, Base.size, Base.+, Base.*, Images.Gray, Images.colorview, Images.RGB
+	export Sandpile, topple!, deposit!, fullyTopple!, isStable, toImageGray, toImageRGB, toImageIndexed
 
 	"Sandpile struct with two constructors"
 	mutable struct Sandpile
 		# Multidim. Array for holding pile data
-		pile::Array{UInt16, 2}
+		pile::Array{UInt32, 2}
 		# Explicit constructor
 		function Sandpile(p::Array{t, 2}) where t <: Integer
 			local i, j
-			local q = similar(p, UInt16)
+			local q = similar(p, UInt32)
 			for i = 1:size(p)[1]
 				for j = 1:size(p)[2]
 					q[i, j] = p[i, j]
@@ -18,7 +18,7 @@ module ccSandpiles
 			new(q)
 		end
 		# Implicit constructor
-		Sandpile(w::Integer, h::Integer, empty::Bool=true) = empty ? new(zeros(UInt16, UInt(w), UInt(h))) : new(Array{UInt16}(undef, UInt(w), UInt(h)))
+		Sandpile(w::Integer, h::Integer, empty::Bool=true) = empty ? new(zeros(UInt16, UInt(w), UInt(h))) : new(Array{UInt32}(undef, UInt(w), UInt(h)))
 	end
 
 	"Determine the maximum cell value in a sandpile"
@@ -97,10 +97,30 @@ module ccSandpiles
 	+(p1::Sandpile, p2::Sandpile) = size(p1) == size(p2) ? Sandpile(p1.pile + p2.pile) : throw(ErrorException("The two piles must be the same dimensions!\n"))
 
 	"Scalar-multiply a sandpile"
-	function *(n::t, p::Sandpile) where t <: Integer
-		Sandpile(UInt16(n) * p.pile)
+	function *(n::Integer, p::Sandpile)
+		Sandpile(UInt32(n) * p.pile)
 	end
 
 	"Determine if a sandpile is stable"
 	isStable(p::Sandpile) = maximum(p) < 4
+
+	"Convert a sandpile to a grayscale image"
+	toImageGray(p::Sandpile) = Gray.(map(n -> round(n / 3, digits = 2), p.pile))
+
+	"Convert a sandpile to an RGB image"
+	function toImageRGB(p::Sandpile, r::Float64 = 0.75, g::Float64 = 0.0, b::Float64 = 1.0)
+		local gray = toImageGray(p)
+		colorview(RGB, r * gray, g * gray, b * gray)
+	end
+
+	"Convert a sandpile to an indexed-RGB image"
+	toImageIndexed(
+		p::Sandpile,
+		colors::Array{RGB, 1} = [
+			RGB(0.2, 0.2, 0.2),
+			RGB(0.25, 0.25, 1.0),
+			RGB(1.0, 0.25, 0.25),
+			RGB(1.0, 1.0, 0.25)
+		]
+	) = map(n -> colors[n + 1], p.pile)
 end
